58262922ce28b39c5605d9c3b4aa965af0f13c24
Fakhri1999 <fakhriimaduddin19@gmail.com>
2022-01-29 19:40:48 +0700

    feat: remove unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  console.log('hello');
   return <Component {...pageProps} />;
 };
 
