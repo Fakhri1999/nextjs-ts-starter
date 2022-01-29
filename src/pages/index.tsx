035df8ab3c5a5bf52189cd24b0383bd65935f372
Fakhri1999 <fakhriimaduddin19@gmail.com>
2022-01-29 13:04:49 +0700

    fix: remove unused file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import { useEffect } from 'react';
 import styles from 'styles/Home.module.css';
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    console.log(1);
-  }, []);
-
   return (
     <div className={styles.container}>
       <Head>
