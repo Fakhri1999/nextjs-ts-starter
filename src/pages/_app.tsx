c8e11ac31d86bd9f537fbd17ac6dc7963a7d2ba6
Fakhri1999 <fakhriimaduddin19@gmail.com>
2022-01-29 19:39:31 +0700

    feat: test husky

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  console.log('hello');
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
