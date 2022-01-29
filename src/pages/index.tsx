4e6b9afbc6db336145e82150f3db4d21de7c844e
Fakhri1999 <fakhriimaduddin19@gmail.com>
2022-01-29 13:02:00 +0700

    feat: test commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-// import styles from 'styles/Home.module.css';
+import { useEffect } from 'react';
+import styles from 'styles/Home.module.css';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    console.log(1);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
