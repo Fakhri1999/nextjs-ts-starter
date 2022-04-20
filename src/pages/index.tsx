4587455193777298fe79803898aeff8b582579cc
Fakhri1999 <fakhriimaduddin19@gmail.com>
2022-04-20 23:25:21 +0700

    feat: add github action and error example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import styles from 'styles/Home.module.css';
 
 const Home: NextPage = () => {
+  const test = 'hello';
   return (
     <div className={styles.container}>
       <Head>
