ec64e59e011e527d9c87104a1db5a79f84bdfbea
Fakhri1999 <fakhriimaduddin19@gmail.com>
2022-09-24 18:00:08 +0700

    feat: test error file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 
 function Home() {
+  const text = 'hello';
   return (
     <div className={styles.container}>
       <Head>
