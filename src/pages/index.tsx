7e10ceff471b661e46c620c42b23b0a620bad90f
Fakhri1999 <fakhriimaduddin19@gmail.com>
2022-09-24 18:08:03 +0700

    feat: fix error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 
 function Home() {
-  const text = 'hello';
   return (
     <div className={styles.container}>
       <Head>
